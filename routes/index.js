43b0f570088c099e082a27edd8a93dc84d7d0fdd
mruz <mruz@gmail.com>
2025-01-19 16:54:09 +0300

    Пункт 4_2

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,19 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* Страница Коржика*/
+router.get('/valli', function(req, res, next) {
+  res.send("<h1>Страница Валли</h1>")
+  });
+
+  /* Страница Карамельки */
+router.get('/eva', function(req, res, next) {
+  res.send("<h1>Страница Валли</h1>")
+  });
+
+  /* Страница Компота */
+  router.get('/kapitan', function(req, res, next) {
+  res.send("<h1>Страница Капитан</h1>")
+  });
+
 module.exports = router;
