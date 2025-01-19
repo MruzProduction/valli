e77dd218c3c0e4948444a61ae78284a0244c8f63
mruz <mruz@gmail.com>
2025-01-19 20:38:44 +0300

    vl_8_3

--- routes/vallis.js
+++ routes/vallis.js
@@ -1,13 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var Valli = require('../models/valli').Valli;
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('Новый маршрутизатор, для маршрутов, начинающихся с vallis');
+router.get('/', function (req, res, next) {
+    res.send('Новый маршрутизатор, для маршрутов, начинающихся с vallis');
 });
 
-router.get("/:nick", function(req, res, next) {
-    res.send(req.params.nick);
-    });
+router.get("/:nick", async function (req, res, next) {
+    var vallis = await Valli.find({ nick: req.params.nick });
+    console.log(vallis)
+    if (!vallis.length) return next(new Error("Нет такого героя в мультфильме Валли"))
+var valli = vallis[0];
+    res.render('valli', {
+        title: valli.title,
+        picture: valli.avatar,
+        desc: valli.desc
+    })
+});
 
 module.exports = router;
