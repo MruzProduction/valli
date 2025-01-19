4aa5b4cfa67fc7ecdec6092cd94331f39b6c48f3
mruz <mruz@gmail.com>
2025-01-19 23:34:22 +0300

    vl_10_1

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var router = express.Router();
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', counter:req.session.counter });
+  req.session.greeting = "Hi!!!";
+  res.render('index', { title: 'Express',
+  counter:req.session.counter });
+  });
+  
+router.get('/logreg', function(req, res, next) {
+  res.render('logreg',{title: 'Вход', error: null});
   });
 module.exports = router;
