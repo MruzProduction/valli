c693d769839470e82a8131f06e1cc09c8abd233e
mruz <mruz@gmail.com>
2025-01-19 19:41:21 +0300

    vl_7_2

--- testMongoose2024.js
+++ testMongoose2024.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/testMongoose2024');
-const Valli = mongoose.model('Valli', { name: String });
-const hero = new Valli({ name: 'Valli' });
-hero.save().then(() => console.log('Зэбзэб')); 
+var schema = mongoose.Schema({ name: String })
+schema.methods.zebzeb = function(){
+console.log(this.name + " сказал зэбзэб")
+}
+const valli = mongoose.model('valli', schema);
+const hero = new valli({ name: 'Валли' });
+hero.save().then(() => hero.zebzeb());
